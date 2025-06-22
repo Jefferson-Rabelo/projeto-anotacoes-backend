c39f677878fa0c53dd78539588efa3e6d13a2fb9
Jefferson Rabelo <jeffersonrabelo1221@gmail.com>
2025-06-22 10:23:44 -0300

    Troca sqlite3 por better-sqlite3 para deploy no Render

--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,14 @@
 module.exports = {
   development: {
     client: "sqlite3",
+
     connection: {
       filename: "./src/database/database.sqlite",
     },
-    useNullAsDefault: true,
+
     migrations: {
       directory: "./src/database/migrations",
     },
+    useNullAsDefault: true,
   },
 };
